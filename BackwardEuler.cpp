54c58371fe3bf5a2f15c3f149731e783e1d5e43a
youngers2006 <sy2224@ic.ac.uk>
2025-12-01 23:26:19 +0000

    added

--- BackwardEuler.cpp
+++ BackwardEuler.cpp
@@ -2,12 +2,14 @@
 void BackwardEuler::step(System& ODE, double dt) {
     // Get current state and time and initialise the state derivative vector.
     std::vector<double> y = ODE.get_state();
-    std::vector<double> dydt(y.size());
     double t = ODE.get_time();
-
-    
-    
-
+    std::vector<double> y_ = NewtonRaphsonSolver(
+        ODE, 
+        y, 
+        dt, 
+        1e-9, 
+        40
+    );
     ODE.set_time(t + dt);
     ODE.set_state(y_);
 };
