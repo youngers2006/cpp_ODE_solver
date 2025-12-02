2423f63252be9068fbd530a47b518b113472401a
youngers2006 <sy2224@ic.ac.uk>
2025-12-02 00:39:16 +0000

    added

--- re_entryODE.cpp
+++ re_entryODE.cpp
@@ -20,3 +20,32 @@ void re_entry_ODE::f(
                 dydt[1] = v_;
                 dydt[2] = gamma_;
 };
+
+void re_entry_ODE::jac(
+            const std::vector<double>& y,
+            double t,
+            std::vector<std::vector<double>>& jac
+        ) {
+            double h = y[0];
+            double v = y[1];
+            double gamma = y[2];
+
+            double rho0 = parameters[0];
+            double Cl = parameters[1];
+            double Cd = parameters[2];
+            double m = parameters[3];
+            double g = parameters[4];
+            double A = parameters[5];
+
+            jac[0][0] = 0;
+            jac[0][1] = - sin((3.1415 / 180) * gamma);
+            jac[0][2] = - v * cos((3.1415 / 180) * gamma);
+
+            jac[1][0] = (rho0 * pow(v,2) * Cd * A * exp(-h / 7200.0)) / (2 * 7200.0 * m);
+            jac[1][1] = (rho0 * Cd * A * v * exp(-h / 7200.0)) / m;
+            jac[1][2] = -g * cos((3.1415 / 180) * gamma);
+
+            jac[2][0] = (-rho0 * Cd * A * exp(-h / 7200)) / (2 * 7200 * m);
+            jac[2][1] = ((rho0 * A * Cd * exp(-h / 7200)) / (2 * m)) + ((g * cos((3.1415 / 180) * gamma)) / pow(v, 2));
+            jac[2][2] = (g / v) * sin((3.1415 / 180) * gamma);
+};
