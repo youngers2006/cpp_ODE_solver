ddd2766ed5026659fb5747b2a19cdef05d7b965d
youngers2006 <sy2224@ic.ac.uk>
2025-12-12 17:15:47 +0000

    added

--- ground_truth_entry.m
+++ ground_truth_entry.m
@@ -1,9 +1,6 @@
 clc
 clear
 
-run()
-
-function run()
 rho0 = 1.225;
 h0 = 100000.0;
 v0 = 1000.0;
@@ -11,12 +8,12 @@ gamma0 = 180.0;
 gamma0 = (pi / 180.0) * gamma0;
 y0 = [h0; v0; gamma0];
 Cl = 0.6;
-Cd = 0.8;
+Cd = 1.2;
 m = 100.0;
 g = 9.81;
 S = 0.5;
 
-T = [0 1000];
+T = [0 500];
 
 ode_fun = @(t, y) reentry_equations(y, rho0, Cl, Cd, m, g, S);
 options = odeset('RelTol', 1e-6, 'AbsTol', 1e-6);
@@ -34,18 +31,32 @@ xlabel('Time (s)');
 ylabel('m / ms^-1 / radians');
 legend('show');
 grid on;
-end
 
 function dydt = reentry_equations(y, rho0, Cl, Cd, m, g, S)
-h = y(1);
-v = y(2);
-gamma = y(3);
-rho = rho0 * exp(-h / 7200);
-dyn_p = 0.5 * rho * (v ^ 2);
-D = Cd * S * dyn_p;
-L = Cl * S * dyn_p;
-h_dot = v .* sin(gamma);
-v_dot = - (D / m) - g .* sin(gamma);
-gamma_dot = - (L / (m .* v)) - (g .* cos(gamma)) ./ v;
-dydt = [h_dot; v_dot; gamma_dot];
-end 
+    % Inputs
+    h = y(1); v = y(2); gamma_deg = y(3); 
+    
+    % Constants from your C++ code
+    deg_to_rad = 3.14159265 / 180.0;
+    rad_to_deg = 180.0 / 3.14159265;
+    gamma_rad = gamma_deg * deg_to_rad;
+    
+    % Forces
+    rho = rho0 * exp(-h / 7200.0);
+    dyn_p = 0.5 * rho * S * v^2;
+    L = dyn_p * Cl;
+    D = dyn_p * Cd;
+    
+    % Equations matching your re_entryODE.cpp exactly:
+    % h_dot = -v * sin(gamma)
+    h_dot = -v * sin(gamma_rad);
+    
+    % v_dot = -(D/m) + g * sin(gamma)
+    v_dot = -(D/m) + g * sin(gamma_rad);
+    
+    % gamma_dot in deg/s
+    gamma_dot_rad = (L / (m * v)) + (g / v) * cos(gamma_rad);
+    gamma_dot = gamma_dot_rad * rad_to_deg;
+    
+    dydt = [h_dot; v_dot; gamma_dot];
+end
--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main() {
     bool file_exists = (response == 'y' || response == 'Y');
 
     std::string input_filename = "input_file.txt"; 
-    std::string output_filename = "RK4_sat_5.txt"; 
+    std::string output_filename = "IE_RE_3.txt"; 
     Solver ODE_solver;
 
     if (!file_exists) {
